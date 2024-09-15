f4040a712bd63dd24d488169b85fbba4177a11e5
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-09-15 23:50:30 +0530

    testing sec colors & TWcss animations added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
     extend: {
       colors:{
         primary: '#F3D8C7',
-        secondary: '#78CCA2'
+        secondary: '#534439',
+        secondary_1: '#086a5e',
+        secondary_2: '#443124',
+        secondary_3: '#534439',
+        secondary_4: '#402e32',
+        secondary_5: '#00755f'
       }
     },
   },
