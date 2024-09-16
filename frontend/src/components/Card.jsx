a2609079fafc6e322df4819cf3cfd063b9f3959a
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-09-16 13:17:33 +0530

    Update Card.jsx

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 function Card() {
   return (
-    <div className='w-60 h-80 border-2 border-green-500 '>
-      <div>
-        <img
-          className='object-cover w-full h-full'
-          src='https://placehold.co/600x400.png'
-          alt=''
-        />
-        <div className='p-4 text-center'>
-          <h2 className='text-xl font-bold'>Product Name</h2>
-          <p className='text-gray-600'>Lorem ipsum dolor sit amet.</p>
-          <p className='text-gray-400'>$99.99</p>
-        </div>
+    <div className="w-9/12 h-9/12  bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105">
+      <img
+        className="object-cover w-full h-2/3"
+        src="https://placehold.co/400x400.png"
+        alt="Product"
+      />
+      <div className="p-4 text-center">
+        <h2 className="text-xl font-bold text-gray-800">Product Name</h2>
+        <p className="text-gray-600 mt-1">Lorem ipsum dolor sit amet.</p>
+        <p className="text-gray-400 mt-1">$99.99</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
