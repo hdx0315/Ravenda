53a44b73768138871d6db7c2be1a0151c9aa1ead
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-09-20 20:30:38 +0530

    Update NewArrivals.jsx

--- frontend/src/pages/NewArrivals.jsx
+++ frontend/src/pages/NewArrivals.jsx
@@ -1,8 +1,74 @@
 import React from 'react'
+import Card from '../components/Card';
+import NavBar from '../components/NavBar';
 
 function NewArrivals() {
   return (
-    <div>NewArrivals</div>
+    
+        <div className="min-h-fit font-main">
+          
+          
+        <NavBar/>
+          {/* Title Section */}
+          <div className="text-center pt-24">
+            <h2 className="text-4xl sm:text-5xl font-bold text-gray-800 font-main mb-4">
+              New Arrivals
+            </h2>
+            <p className="text-gray-600 text-lg sm:text-2xl">
+              Discover the latest trends fresh off the runway.
+            </p>
+          </div>
+    
+          <div className="flex sm:hidden my-8">
+            <div className='flex flex-wrap gap-8 m-8 justify-center items-center'>
+              <Card />
+              <Card />
+              <Card />
+            </div>
+          </div>
+          
+          <div className="flex sm:hidden my-8">
+            <div className='flex flex-wrap gap-8 m-8 justify-center items-center'>
+              <Card />
+              <Card />
+              <Card />
+            </div>
+          </div>
+    
+          <div className="flex sm:hidden my-8">
+            <div className='flex flex-wrap gap-8 m-8 justify-center items-center'>
+              <Card />
+              <Card />
+              <Card />
+            </div>
+          </div>
+    
+    
+
+          <div className="hidden sm:flex  my-8">
+        <div className='flex flex-wrap gap-8 m-8 justify-center items-center'>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className='hidden md:flex flex-wrap gap-8 m-8  justify-center items-center'>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className='hidden lg:flex flex-wrap gap-8 m-8  justify-center items-center'>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+
+    
+        </div>
+    
+
+
+
   )
 }
 
