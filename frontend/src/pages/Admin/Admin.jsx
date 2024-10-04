67ec14b1465dc7a819101952a522a8ef86edb7b3
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-10-04 23:38:24 +0530

    confls

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -1,24 +1,6 @@
 
 
-import React, { useState } from 'react'
-import { imgStorage, dataStorage, ref } from '../../../../backend/firebase/firebase-config'
-
-
-
-import React, {  } from 'react';
-
-    const handleUpload = (e) => {
-        //setImg(e.target.files[0])
-        console.log(e.target.files[0])
-
-
-        const images = ref(imgStorage, 'images/test/')
-
-        uploadbytes(images, e.target.files[0]).then(data=> {
-          console.log('Uploaded a blob:', data.ref.name, data)
-        })
-
-    }
+import React from 'react'
 
 function Admin() {
     return(
