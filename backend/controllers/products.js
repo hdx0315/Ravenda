2a3c2d6ccd279021aadb5a9f8e650e436ac83242
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-10-07 23:14:31 +0530

    Update products.js

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -2,7 +2,6 @@
 
 // backend/controllers/products.js
 
-
 const {Detail} = require('../models/admin')
 
 const getProducts = async (req, res) => {
@@ -18,4 +17,17 @@ const getProducts = async (req, res) => {
     }
 }
 
-module.exports = {getProducts}
+const getSingleProduct = async (req, res) =>{
+
+    try{
+        const product = await Detail.findById(req.params.id);
+
+        res.status(200).json(product);
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({message: 'Failed to fetch product', error});
+    }
+}
+
+module.exports = {getProducts, getSingleProduct}
