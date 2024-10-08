012c5cd2230188fcf046041b327ee07d095b035f
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-10-09 03:28:34 +0530

    new assets

---
+++ frontend/src/assets/images/new/assets-new-images.js
@@ -0,0 +1,20 @@
+import t1 from './Casual Cami Dress With Ruched Waistline.jpg'
+import t2 from './Contrast Binding Ribbed Crop Tee.jpg'
+import t3 from './cotton-linen shorts.jpg'
+import t4 from './Floral puff sleeve midi dress.jpg'
+
+import b1 from './Hem Long Sleevless Elegant Dress.jpg'
+import b2 from './Kaia top.jpg'
+
+import f1 from './Light Blue High Waist Summer Straight Pants.jpg'
+import f2 from './Off Shoulder Ruched Mesh Green Top.jpg'
+import f3 from './Pleated Waist Cami Dress.jpg'
+import f4 from './Scoop Neck Ruched Long Sleeves Red Crop Tee.jpg'
+
+const images = {
+    t1,t2,t3,t4,
+    b1,b2,
+    f1,f2,f3,f4
+}
+
+export { images };
