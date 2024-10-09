6e87e44dca0f1ab87a7c632d979217666d614d19
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-10-10 02:29:50 +0530

    assets updates

--- frontend/src/assets/images/new/assets-new-images.js
+++ frontend/src/assets/images/new/assets-new-images.js
@@ -1,15 +1,15 @@
-import t1 from './Casual Cami Dress With Ruched Waistline.jpg'
-import t2 from './Contrast Binding Ribbed Crop Tee.jpg'
-import t3 from './cotton-linen shorts.jpg'
-import t4 from './Floral puff sleeve midi dress.jpg'
+import f1 from './Casual Cami Dress With Ruched Waistline.jpg'
+import f2 from './Floral puff sleeve midi dress.jpg'
+import f3 from './Hem Long Sleevless Elegant Dress.jpg'
+import f4 from './Pleated Waist Cami Dress.jpg'
 
-import b1 from './Hem Long Sleevless Elegant Dress.jpg'
-import b2 from './Kaia top.jpg'
+import t1 from './Contrast Binding Ribbed Crop Tee.jpg'
+import t2 from './Kaia top.jpg'
+import t3 from './Off Shoulder Ruched Mesh Green Top.jpg'
+import t4 from './Scoop Neck Ruched Long Sleeves Red Crop Tee.jpg'
 
-import f1 from './Light Blue High Waist Summer Straight Pants.jpg'
-import f2 from './Off Shoulder Ruched Mesh Green Top.jpg'
-import f3 from './Pleated Waist Cami Dress.jpg'
-import f4 from './Scoop Neck Ruched Long Sleeves Red Crop Tee.jpg'
+import b1 from './cotton-linen shorts.jpg'
+import b2 from './Light Blue High Waist Summer Straight Pants.jpg'
 
 const images = {
     t1,t2,t3,t4,
